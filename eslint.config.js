df0425ed234bd4a14f4184c7acc0f5ed7a4de3cf
Matthew Tole <matthewtole@gmail.com>
2026-01-17 20:03:34 -0800

    refactor: clean up code formatting and import statements across multiple files

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,5 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
+import storybook from 'eslint-plugin-storybook'
 
 import js from '@eslint/js'
 import react from 'eslint-plugin-react'
@@ -8,39 +8,44 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import prettier from 'eslint-plugin-prettier/recommended'
 
-export default tseslint.config({ ignores: ['dist', 'styled-system', '.vite', '.storybook-static'] }, {
-  extends: [js.configs.recommended, ...tseslint.configs.recommended],
-  files: ['**/*.{ts,tsx}'],
-  languageOptions: {
-    ecmaVersion: 2022,
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true,
+export default tseslint.config(
+  { ignores: ['dist', 'styled-system', '.vite', '.storybook-static'] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+        sourceType: 'module',
       },
-      sourceType: 'module',
+    },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...react.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'react/react-in-jsx-scope': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_' },
+      ],
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
     },
   },
-  plugins: {
-    react,
-    'react-hooks': reactHooks,
-    'react-refresh': reactRefresh,
-  },
-  rules: {
-    ...react.configs.recommended.rules,
-    ...reactHooks.configs.recommended.rules,
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-    'react/react-in-jsx-scope': 'off',
-    '@typescript-eslint/no-unused-vars': [
-      'error',
-      { argsIgnorePattern: '^_' },
-    ],
-  },
-  settings: {
-    react: {
-      version: 'detect',
-    },
-  },
-}, prettier, storybook.configs["flat/recommended"]);
+  prettier,
+  storybook.configs['flat/recommended']
+)
--- panda.config.ts
+++ panda.config.ts
@@ -8,10 +8,7 @@ export default defineConfig({
   jsxFramework: 'react',
 
   // Where to look for your css declarations
-  include: [
-    './src/**/*.{js,jsx,ts,tsx}',
-    './.storybook/**/*.{js,jsx,ts,tsx}',
-  ],
+  include: ['./src/**/*.{js,jsx,ts,tsx}', './.storybook/**/*.{js,jsx,ts,tsx}'],
 
   // Files to exclude
   exclude: [],
